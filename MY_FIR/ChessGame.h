b6a1d816ba5ec58d15a62ba549b5709f1215dd45
P-rctica <1943117060@qq.com>
2022-11-06 01:39:57 +0800

    ChessGame类接口设计

--- MY_FIR/ChessGame.h
+++ MY_FIR/ChessGame.h
@@ -1,5 +1,23 @@
 #pragma once
+
+//表示落子位置
+struct ChessPos {
+	int row;
+	int col;
+};
+
+typedef enum {
+	CHESS_WHITE = -1,  //白棋
+	CHESS_BLACK = 1
+}chess_kind_t;
+
 class ChessGame
 {
+public:
+	void chessDown(ChessPos* pos, chess_kind_t kind);
+	bool clickBoard(int x, int y, ChessPos *pos);
+	void init();   //初始化
+	int gerGradeSize();  //获取棋盘大小（13， 15， 19）
+	bool checkOver(); //检查棋局是否结束
 };
 
