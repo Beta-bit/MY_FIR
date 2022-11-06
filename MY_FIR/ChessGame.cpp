65ae688d35ef6e899ecefa5d1a5dd194bb0163c3
P-rctica <1943117060@qq.com>
2022-11-06 11:47:49 +0800

    CheesGame实现

--- MY_FIR/ChessGame.cpp
+++ MY_FIR/ChessGame.cpp
@@ -1,5 +1,29 @@
 #include "ChessGame.h"
 
+ChessGame::ChessGame(Man* man, AI* ai, Chess* chess)
+{
+	this->man = man;
+	this->ai = ai;
+	this->chess = chess;
+}
+
+//对局（开始五子棋游戏）
 void ChessGame::play()
 {
+	chess->init();
+	while (1) {
+		//先由棋手走
+		man->go();
+		if (chess->checkOver()) {
+			chess->init();
+			continue;
+		}
+
+		//AI走
+		ai->go();
+		if (chess->checkOver()) {
+			chess->init();
+			continue;
+		}
+	}
 }
