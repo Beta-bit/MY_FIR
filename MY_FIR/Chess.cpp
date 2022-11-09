71da3b8e347150a459de045421cb0813ede3a80e
P-rctica <1943117060@qq.com>
2022-11-09 11:49:44 +0800

    初始化棋盘

--- MY_FIR/Chess.cpp
+++ MY_FIR/Chess.cpp
@@ -1,4 +1,6 @@
 #include "Chess.h"
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
 
 Chess::Chess(int gradeSize, int margin_x, int margin_y, float chessSize)
 {
@@ -19,6 +21,26 @@ Chess::Chess(int gradeSize, int margin_x, int margin_y, float chessSize)
 
 void Chess::init()
 {
+    //创建游戏窗口
+    initgraph(897, 895);
+
+    //显示棋盘图片
+    loadimage(0, "../res/棋盘2.jpg");
+    //播放开始提示音
+    mciSendString("play ../res/start.wav", 0, 0, 0);
+
+    //加载棋子的图片
+    loadimage(&chessBlackImg, "../res/black.png", chessSize, chessSize, true);
+    loadimage(&chessWhiteImg, "../res/white.png", chessSize, chessSize, true);
+
+    //棋盘清零
+    for (int i = 0; i < gradeSize; i++) {
+        for (int j = 0; j < gradeSize; j++) {
+            chessMap[i][j] = 0;
+        }
+    }
+
+    playerFlag = true;
 }
 
 bool Chess::clickBoard(int x, int y, ChessPos* pos)
